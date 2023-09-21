c7c4f3aa4e74260e5c6447ec0999bf7876c64e3b
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-21 23:18:45 +0100

    Formatting dates

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import getFormattedDate from "@/lib/getFormattedDate";
 
 type Props = {
   post: BlogPost;
 };
 export default function ListItem({ post }: Props) {
   const { id, title, date } = post;
+  const formattedDate = getFormattedDate(date);
   return <div>ListItem</div>;
 }
