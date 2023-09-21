fbf99acec63d65bd6cb2052740cc81c67c7feb95
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-21 22:33:23 +0100

    Styling Navbar h1

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,9 @@ export default function Navbar() {
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
       <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
         <Link href="/" className="text-white/90 no-underline hover:text-white ">
-          Philippe
+          <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+            Philippe
+          </h1>
         </Link>
       </div>
     </nav>
