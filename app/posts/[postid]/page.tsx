873c7743945b5867e899bfaf16876614238c5df1
Philippe <55062877+Philippe35507@users.noreply.github.com>
2023-09-23 14:05:40 +0100

    Dynamic Post Pt. 2

--- app/posts/[postid]/page.tsx
+++ app/posts/[postid]/page.tsx
@@ -1,5 +1,7 @@
-import { getSortedPostsData } from "@/lib/posts";
 import { notFound } from "next/navigation";
+import { getSortedPostsData, getPostData } from "@/lib/posts";
+import getFormattedDate from "@/lib/getFormattedDate";
+import Link from "next/link";
 
 export function generateMetadata({ params }: { params: { postId: string } }) {
   const posts = getSortedPostsData();
@@ -25,5 +27,20 @@ export default async function Post({ params }: { params: { postId: string } }) {
     return notFound();
   }
 
-  return <div>page</div>;
+  const { title, date, contentHtml } = await getPostData(postId);
+
+  const pubDate = getFormattedDate(date);
+
+  return (
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+      <p className="mt-0">{pubDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p>
+          <Link href="/">← Back to home</Link>
+        </p>
+      </article>
+    </main>
+  );
 }
